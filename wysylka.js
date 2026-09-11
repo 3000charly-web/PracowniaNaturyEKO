ab78c364691107ad53b41f0ca8257a5ce47148ab
3000charly-web <3000charly@gmail.com>
2026-09-11 14:40:09 +0200

    wersja 3.24

--- wysylka.js
+++ wysylka.js
@@ -1,7 +1,7 @@
 // CENY WYSYŁKI — EDYTUJ TUTAJ
 window.SHIPPING = [
-  { nazwa: "Paczkomat InPost — przedpłata", cena: "19 zł" },
-  { nazwa: "Kurier InPost — przedpłata", cena: "22 zł" },
-  { nazwa: "Inny kurier", cena: "35 zł" },
+  { nazwa: "Paczkomat InPost — przedpłata", cena: "22 zł" },
+  { nazwa: "Kurier InPost — przedpłata", cena: "29 zł" },
+  { nazwa: "Inny kurier", cena: "39 zł" },
   { nazwa: "Odbiór osobisty — po wcześniejszym uzgodnieniu", cena: "bezpłatnie" }
 ];
